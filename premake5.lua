8ac67b7bb8ee4ab8910daf170765435abd3be3c3
loganr3093 <loganr3093@gmail.com>
2024-06-19 22:20:03 -0700

    Fix premake5 file

--- premake5.lua
+++ premake5.lua
@@ -1,137 +1,28 @@
-workspace "Voxen"
-	architecture "x64"
-
-	configurations
-	{
-		"Debug",
-		"Release",
-		"Dist"
-	}
-
-outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
-
--- Include directories relative to root folder (solution directory)
-IncludeDir = {}
-IncludeDir["GLFW"] = "Voxen/vendor/GLFW/include"
-IncludeDir["Glad"] = "Voxen/vendor/Glad/include"
-IncludeDir["ImGui"] = "Voxen/vendor/imgui"
-
-include "Voxen/vendor/GLFW"
-include "Voxen/vendor/Glad"
-include "Voxen/vendor/imgui"
-
-project "Voxen"
-	location "Voxen"
-	kind "SharedLib"
-	language "C++"
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	pchheader "voxpch.h"
-	pchsource "Voxen/src/voxpch.cpp"
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include",
-		"%{IncludeDir.GLFW}",
-		"%{IncludeDir.Glad}",
-		"%{IncludeDir.ImGui}"
-	}
-
-	links
-	{
-		"GLFW",
-		"Glad",
-		"ImGui",
-		"opengl32.lib"
-	}
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp"
+    }
 
 	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
+        systemversion "latest"
+        cppdialect "C++20"
+        staticruntime "On"
 
-		defines
-		{
-			"VOX_PLATFORM_WINDOWS",
-			"VOX_BUILD_DLL",
-			"GLFW_INCLUDE_NONE"
-		}
-
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
-		}
-
-	filter "configurations:Debug"
-		defines "VOX_DEBUG"
-		buildoptions "/MDd"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "VOX_RELEASE"
-		buildoptions "/MD"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "VOX_DIST"
-		buildoptions "/MD"
-		optimize "On"
-
-project "Sandbox"
-	location "Sandbox"
-	kind "ConsoleApp"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"Voxen/vendor/spdlog/include",
-		"Voxen/src"
-	}
-
-	links
-	{
-		"Voxen"
-	}
-
-	filter "system:windows"
-		cppdialect "C++20"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"VOX_PLATFORM_WINDOWS"
-		}
-
-	filter "configurations:Debug"
-		defines "VOX_DEBUG"
-		buildoptions "/MDd"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "VOX_RELEASE"
-		buildoptions "/MD"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "VOX_DIST"
-		buildoptions "/MD"
-		optimize "On"
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
